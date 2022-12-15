4331bdad66fc2de3860f75774981b6a5f1e4d467
Isabella Rodrigues <bebellabr@gmail.com>
2022-12-15 16:48:16 -0300

    Creating basic system

--- Project One/complete_guide/lib/main.dart
+++ Project One/complete_guide/lib/main.dart
@@ -3,14 +3,41 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestions() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "what's your favorite color?",
+      "What's your favorite animal?",
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Text("This is my default text"),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            ElevatedButton(child: Text("Answer 1"), onPressed: answerQuestions),
+            ElevatedButton(
+              child: Text("Answer 2"),
+              onPressed: answerQuestions,
+            ),
+            ElevatedButton(
+              child: Text("Answer 3"),
+              onPressed: answerQuestions,
+            ),
+          ],
+        ),
       ),
     );
   }
